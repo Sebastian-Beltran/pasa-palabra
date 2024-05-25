91f9528ff5ffdf0de9606af833bb31c575ebde9f
Sebastian Beltran <beltran.sebastiang@gmail.com>
2024-05-24 23:39:53 -0500

    Added colors and home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pasa palabra',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const primaryColor = Color.fromRGBO(19, 17, 66, 1);
+  static const secondaryColor = Color.fromRGBO(0, 139, 156, 1);
+}
